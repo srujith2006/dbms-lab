b5e5882e8d3357a61b2bd1333dba5f3fd29fe1fb
srujith2006 <srujithraj06@gmail.com>
2025-11-05 20:18:06 +0530

    Update createInsertSelect.sql

--- createAndInsert.sql
+++ createAndInsert.sql
@@ -38,3 +38,53 @@ INSERT INTO Reserves VALUES
 (3, 103, '2024-05-12'),
 (1, 104, '2024-05-13'),
 (4, 105, '2024-05-14');
+
+
+
+SELECT DISTINCT sname
+FROM Sailors s
+JOIN Reserves r ON s.sid = r.sid;
+
+
+SELECT sname
+FROM Sailors
+WHERE sid IN (
+  SELECT r.sid
+  FROM Reserves r
+  WHERE r.bid IN (
+    SELECT b.bid FROM Boats b WHERE b.color = 'red'
+  )
+);
+
+
+SELECT s.sname, b.bname
+FROM Sailors s
+LEFT JOIN Reserves r ON s.sid = r.sid
+LEFT JOIN Boats b ON r.bid = b.bid;
+
+
+SELECT rating, sname, age
+FROM Sailors s
+WHERE age = (
+  SELECT MAX(age)
+  FROM Sailors
+  WHERE rating = s.rating
+);
+
+
+SELECT DISTINCT s.sname
+FROM Sailors s
+JOIN Reserves r ON s.sid = r.sid
+JOIN Boats b ON r.bid = b.bid
+WHERE b.color = 'red';
+
+SELECT s.sname
+FROM Sailors s
+JOIN Reserves r ON s.sid = r.sid
+JOIN Boats b ON r.bid = b.bid
+WHERE b.color IN ('red', 'green')
+GROUP BY s.sname
+HAVING COUNT(DISTINCT b.color) = 2;
+
+SELECT * FROM Sailors
+WHERE sname LIKE 'B%';
