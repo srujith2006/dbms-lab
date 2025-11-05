36363eba9399d2b3dac7681963b953c16d81b596
srujith2006 <srujithraj06@gmail.com>
2025-11-05 20:23:34 +0530

    Update and rename DCL.sql to AggregateAndDCL.sql

--- AggregateAndDCL.sql
+++ AggregateAndDCL.sql
@@ -1,3 +1,16 @@
+CREATE TABLE Sailors (
+  sid INT PRIMARY KEY,
+  sname VARCHAR(30),
+  rating INT,
+  age REAL
+);
+INSERT INTO Sailors VALUES
+(1, 'John', 5, 25),
+(2, 'Bob', 7, 30),
+(3, 'Alice', 8, 29),
+(4, 'Mark', 5, 22),
+(5, 'Ben', 6, 35);
+
 SELECT
   COUNT(*) AS TotalSailors,
   AVG(age) AS AvgAge,
